bf3dd96b88a6c4f4c2db1e90b923d6195876abcb
Kevin Burk <xavierholt@gmail.com>
2017-11-05 12:59:40 -0800

    Only connect to nodes beneath you.

--- snapshot/connector.rb
+++ snapshot/connector.rb
@@ -19,29 +19,25 @@ class Connector
 
   def init_connections!
     threads = []
-    threads << Thread.new { connect }
     threads << Thread.new { listen }
+    threads << Thread.new do
+      (0...@pid).each do |peer_pid|
+        connect(peer_pid)
+      end
+    end
+
     threads.each { |t| t.join }
   end
 
-  def connect
-    peer_pid = -1
-    while @peers.length < @peer_count
-      peer_pid += 1
-      peer_pid = 0 if peer_pid > @peer_count
-      next if peer_pid == @pid
-      peer_port = peer_pid + BASE_PORT
-      begin
-        peer = TCPSocket.open("localhost", peer_port)
-        peer.puts(@pid)
-        add_peer(peer, peer_pid)
-      rescue Errno::ECONNREFUSED
-        sleep rand
-      rescue Exception => e
-        @client.log e
-        sleep rand
-      end
-    end
+  def connect(peer_pid)
+    peer = TCPSocket.open("localhost", BASE_PORT + peer_pid)
+    peer.puts(@pid)
+    add_peer(peer, peer_pid)
+  rescue Errno::ECONNREFUSED
+    sleep rand
+  rescue Exception => e
+    @client.log e
+    sleep rand
   end
 
   def listen
@@ -58,15 +54,17 @@ class Connector
         @client.log e
       end
     end
+  ensure
     server.close
   end
 
   def add_peer(peer, peer_pid)
-    if @peers.length < @peer_count && !@peers.key?(peer_pid)
+    if @peers.key?(peer_pid)
+      @client.log "already connected to client #{peer_pid}"
+      peer.close
+    else
       @client.log "connected to client #{peer_pid}"
       @peers[peer_pid] = peer
-    else
-      peer.close
     end
   end
 
