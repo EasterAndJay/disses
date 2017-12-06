c634614db768c4f4bec3cb461fd1dbff8a6625e6
Kevin Burk <xavierholt@gmail.com>
2017-12-05 21:56:18 -0800

    Getting closer...

--- paxos/client.go
+++ paxos/client.go
@@ -1,35 +1,118 @@
 package main
 
-import "container/list"
 import "fmt"
 import "github.com/golang/protobuf/proto"
 import "net"
 import "time"
 
 type Client struct {
-  port int;
-  sock *net.UDPConn;
-  work chan *Message;
-  logs list.List;
+  // Connection Stuff
+  port      int32;
+  sock      *net.UDPConn;
+  work      chan *Message;
+
+  // Paxos Stuff
+  ballotNum int32;
+  acceptNum int32;
+  acceptVal int32;
+  peers     map[int32]bool;
+  okays     map[int32]bool;
+  logs      []int32;
+
+  // Soccer Stuff
+  remainder int32;
+}
+
+func NewClient(port int32) Client {
+  return Client {
+    port:      port,
+    sock:      nil,
+    work:      make(chan *Message, 16),
+
+    ballotNum: 0,
+    acceptNum: 0,
+    acceptVal: 0,
+    peers:     make(map[int32]bool),
+    okays:     make(map[int32]bool),
+    logs:      make([]int32, 0),
+
+    remainder: 100,
+  }
+}
+
+func (client *Client) Broadcast(message *Message) {
+  for peer, _ := range client.peers {
+    client.Send(peer, message)
+  }
+}
+
+func (client *Client) Commit() {
+  if client.acceptVal < -5000 {
+    // Remove a server that's leaving.
+    delete(client.peers, -client.acceptVal)
+  } else if client.acceptVal > 5000 {
+    // Add a new server.
+    client.peers[client.acceptVal] = true
+  } else {
+    // Adjust the remaining tickets.
+    client.remainder += client.acceptVal
+  }
+
+  client.logs = append(client.logs, client.acceptVal)
+  client.ballotNum = 0
+  client.acceptNum = 0
+  client.acceptVal = 0
+}
+
+func (client *Client) GetEpoch() int32 {
+  return int32(len(client.peers))
+}
+
+func (client *Client) GetID() int32 {
+  return client.port
 }
 
 func (client *Client) Handle() {
   for {
     message := <-client.work
+    epoch := message.GetEpoch()
 
-    epoch = message.GetEpoch()
-    if epoch < logs.Len() {
-      logs.Get(epoch).handle(message)
-    }
-    else if epoch == logs.Len() {
-      logs.PushBack(NewEntry(message))
-    }
-    else {
-      // Way in the future!
-    }
+    fmt.Printf("Got a message! {%v}\n", message)
 
-
-    
+    if epoch < client.GetEpoch() {
+      // It's old. Reply with a NOTIFY.
+      if message.GetType() != Message_NOTIFY {
+        client.Send(message.GetNode(), &Message {
+          Type:  Message_NOTIFY,
+          Epoch: message.GetEpoch(),
+          Value: client.logs[message.GetEpoch()],
+        })
+      }
+    } else if epoch > client.GetEpoch() {
+      // We're out of date!
+      // Dummy proposition to force an update.
+      client.Send(message.GetNode(), &Message {
+        Type:  Message_PROPOSE,
+        Epoch: client.GetEpoch(),
+      })
+    } else {
+      switch message.GetType() {
+      case Message_PETITION:
+        client.HandlePETITION(message)
+      case Message_PROPOSE:
+        client.HandlePROPOSE(message)
+      case Message_PROMISE:
+        client.HandlePROMISE(message)
+      case Message_ACCEPT:
+        client.HandleACCEPT(message)
+      case Message_ACCEPTED:
+        client.HandleACCEPTED(message)
+      case Message_NOTIFY:
+        client.HandleNOTIFY(message)
+      default:
+        fmt.Printf("Unknown message type: %v\n", message)
+      }
+    }
   }
 }
 
@@ -37,7 +120,7 @@ func (client *Client) Listen() {
   buffer := make([]byte, 2048)
   sock, err := net.ListenUDP("udp", &net.UDPAddr {
     IP:   net.ParseIP("127.0.0.1"),
-    Port: client.port,
+    Port: int(client.port),
   })
 
   if err != nil {
@@ -71,23 +154,21 @@ func (client *Client) Run() {
   go client.Listen()
 
   for {
-    client.Send(&Message {
-      Type:  Message_S_PROPOSE,
-      Node:  uint32(client.port),
-      Epoch: 0,
-      Value: 0,
-    }, client.port)
-
     time.Sleep(time.Second)
+    client.Send(client.port, &Message {
+      Type:  Message_PETITION,
+      Value: 10,
+    })
   }
 }
 
-func (client *Client) Send(message *Message, port int) {
+func (client *Client) Send(port int32, message *Message) {
   if client.sock == nil {
     fmt.Printf("Socket not yet open\n")
     return
   }
 
+  message.Node = client.GetID()
   buffer, err := proto.Marshal(message)
   if err != nil {
     fmt.Printf("Marshal error: %v\n", err)
@@ -96,20 +177,11 @@ func (client *Client) Send(message *Message, port int) {
 
   _, err = client.sock.WriteToUDP(buffer, &net.UDPAddr {
     IP:   net.ParseIP("127.0.0.1"),
-    Port: port,
+    Port: int(port),
   })
 
   if err != nil {
     fmt.Printf("Send error: %v\n", err)
     return
-  }
-}
-
-func NewClient(port int) Client {
-  return Client {
-    port: port,
-    sock: nil,
-    work: make(chan *Message, 16),
-    logs: list.New(),
   }
 }
---
+++ paxos/handlers.go
@@ -0,0 +1,76 @@
+package main
+
+import "fmt"
+
+func (client *Client) HandlePETITION(message* Message) {
+  if message.GetValue() < 0 && message.GetValue() > -5000 {
+    if client.remainder + message.GetValue() < 0 {
+      // TODO: Reject!
+    }
+  }
+
+  client.Broadcast(&Message {
+    Type:   Message_PROPOSE,
+    Epoch:  client.GetEpoch(),
+    Ballot: client.ballotNum + 1,
+    Value:  message.GetValue(),
+  })
+}
+
+func (client *Client) HandlePROPOSE(message *Message) {
+  if message.GetBallot() > client.ballotNum {
+    client.ballotNum = message.GetBallot()
+    client.Send(message.GetNode(), &Message {
+      Type:   Message_PROMISE,
+      Epoch:  message.GetEpoch(),
+      Ballot: message.GetBallot(),
+    })
+  } else {
+    fmt.Printf("Ignoring PROPOSE: %v\n", message)
+  }
+}
+
+func (client *Client) HandlePROMISE(message* Message) {
+  // if client.state != PROPOSING {
+  //   return
+  // }
+
+  client.okays[message.GetNode()] = true
+  if len(client.okays) > len(client.peers) / 2 {
+    //TODO Yay acceptance
+  }
+}
+
+func (client *Client) HandleACCEPT(message* Message) {
+  client.acceptNum = message.GetBallot()
+  client.acceptVal = message.GetValue()
+
+  client.Send(message.GetNode(), &Message {
+    Type:   Message_ACCEPTED,
+    Epoch:  message.GetEpoch(),
+    Ballot: message.GetBallot(),
+  })
+}
+
+func (client *Client) HandleACCEPTED(message* Message) {
+  // if client.state != ACCEPTING {
+  //   return
+  // }
+
+  client.okays[message.GetNode()] = true
+  if len(client.okays) > len(client.peers) / 2 {
+    client.Commit()
+
+    client.Broadcast(&Message {
+      Type:   Message_NOTIFY,
+      Epoch:  message.GetEpoch(),
+      Ballot: message.GetBallot(),
+    })
+  }
+}
+
+func (client *Client) HandleNOTIFY(message* Message) {
+  client.acceptNum = message.GetBallot()
+  client.acceptVal = message.GetValue()
+  client.Commit()
+}
