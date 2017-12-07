43da26baded5a1ad794149bdd4dbc1ae3e5f0383
Jonathan Easterman <jonathaneasterman@gmail.com>
2017-12-07 11:40:16 -0800

    Enable cluster size to start with 3 and grow to 5 via cli option

--- paxos/main.go
+++ paxos/main.go
@@ -1,6 +1,7 @@
 package main
 
 import(
+  "flag"
   "log"
   "net"
   "strings"
@@ -10,6 +11,11 @@ import(
 const PEERS_FILE = "peers.txt"
 
 func main() {
+  var clusterSize int
+  flag.IntVar(&clusterSize, "cluster-size", 3, "Enter the cluster size. Valid values are one of: {3, 5}")
+  if clusterSize != 3 || clusterSize != 5 {
+    log.Fatal("Invalid cluster size")
+  }
   ipPorts, err := readLines(PEERS_FILE)
   if err != nil {
     log.Fatal("Error reading configuration file")
@@ -19,7 +25,7 @@ func main() {
     log.Fatal("Error invalid port on first line of configuration file")
   }
   peers := make(map[int32]*net.UDPAddr)
-  for _, ipPort := range ipPorts[1:] {
+  for _, ipPort := range ipPorts[1:clusterSize+1] {
     ip := net.ParseIP(strings.Split(ipPort, ":")[0])
     port, err := strconv.Atoi(strings.Split(ipPort, ":")[1])
     if err != nil {
